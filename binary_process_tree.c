ea4d41b21e9c808036d9df4454b73ce4f4aec16b
Burak Shen <tobuba2@gmail.com>
2019-10-14 23:26:12 +0300

    code format

--- binary_process_tree.c
+++ binary_process_tree.c
@@ -3,24 +3,27 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-void getHeight(int *h){
+void getHeight(int *h)
+{
   printf("Height (max. 12): ");
-  scanf("%d",h);
-  if(*h>12)
+  scanf("%d", h);
+  if (*h > 12)
     *h = 12;
   printf("\n");
 }
 
-void leaf(){
+void leaf()
+{
   char filename[15];
   sprintf(filename, "%d.tmp", getpid());
   FILE *fp = fopen(filename, "w");
   srand(getpid());
-  fprintf(fp, "%d", rand()%100);
+  fprintf(fp, "%d", rand() % 100);
   fclose(fp);
 }
 
-int parent(int pid, int pid2){
+int parent(int pid, int pid2)
+{
   wait(NULL);
   char filename[15];
   sprintf(filename, "%d.tmp", pid);
@@ -36,37 +39,46 @@ int parent(int pid, int pid2){
   fscanf(fp2, "%d", &right);
   fclose(fp1);
   fclose(fp2);
-  fp3 = fopen(filename3,"w");
-  fprintf(fp3, "%d", left+right);
+  fp3 = fopen(filename3, "w");
+  fprintf(fp3, "%d", left + right);
   fclose(fp3);
-  printf("parent(%d): %d, left(%d): %d, right(%d): %d\n", getpid(), left+right, pid, left, pid2, right);
+  printf("parent(%d): %d, left(%d): %d, right(%d): %d\n", getpid(), left + right, pid, left, pid2, right);
 }
 
-void create_tree(int height){
-  if(height == 0){
+void create_tree(int height)
+{
+  if (height == 0)
+  {
     leaf();
     return;
   }
   int pid = fork();
-  if(pid > 0){
+  if (pid > 0)
+  {
     int pid2 = fork();
-    if(pid2 > 0){
+    if (pid2 > 0)
+    {
       //parent
       wait(NULL);
       printf("parent: %d, c1: %d, c2: %d\n", getpid(), pid, pid2);
       fflush(stdout);
       parent(pid, pid2);
-    }else if(pid2 == 0){
+    }
+    else if (pid2 == 0)
+    {
       // child1
       create_tree(--height);
     }
-  }else if(pid == 0){
+  }
+  else if (pid == 0)
+  {
     // child2
     create_tree(--height);
   }
 }
 
-int main(){
+int main()
+{
   int h;
   getHeight(&h);
   create_tree(h);
--- tree.c
+++ tree.c
@@ -4,25 +4,28 @@
 //#include <time.h>
 #include <sys/wait.h>
 
-void getHeight(int *h){
+void getHeight(int *h)
+{
   printf("Height (max. 12): ");
-  scanf("%d",h);
-  if(*h>12)
+  scanf("%d", h);
+  if (*h > 12)
     *h = 12;
   printf("\n");
 }
 
-void createFileChild(int *pid){
+void createFileChild(int *pid)
+{
   srand(*pid);
   char filename[70];
   sprintf(filename, "%d.tmp", *pid);
   FILE *fp;
   fp = fopen(filename, "w+");
-  fprintf(fp, "%d", rand()%100);
+  fprintf(fp, "%d", rand() % 100);
   fclose(fp);
 }
 
-void createFileParent(int *pid, int *childPid){
+void createFileParent(int *pid, int *childPid)
+{
   char childFilename[70];
   sprintf(childFilename, "%d.tmp", *childPid);
   char filename[70];
@@ -32,44 +35,53 @@ void createFileParent(int *pid, int *childPid){
   int c;
   fscanf(fp, "%d", &c);
   fclose(fp);
-  printf("child(%d):%d\n",*childPid,c);
+  printf("child(%d):%d\n", *childPid, c);
   int p = 0;
-  if(access(filename, F_OK) != -1){
+  if (access(filename, F_OK) != -1)
+  {
     fp = fopen(filename, "r");
     fscanf(fp, "%d", &p);
     fclose(fp);
   }
-  printf("parent(%d):%d\n",*pid,p);
+  printf("parent(%d):%d\n", *pid, p);
   fp = fopen(filename, "w");
-  fprintf(fp, "%d", c+p);
+  fprintf(fp, "%d", c + p);
   fclose(fp);
-  printf("parent + child:%d\n",c+p);
+  printf("parent + child:%d\n", c + p);
   printf("==================\n");
 }
 
-void createProcess(){
+void createProcess()
+{
   pid_t pid;
   pid = fork();
-  if(pid>0){
+  if (pid > 0)
+  {
     // parent
     wait(NULL);
     int _pid = getpid();
     //printf("PARENT---%d child_pid:%d\n", _pid, pid);
     createFileParent(&_pid, &pid);
-  }else if(pid==0){
+  }
+  else if (pid == 0)
+  {
     // child
     int _pid = getpid();
     //printf("CHILD---%d\n", _pid);
     createFileChild(&_pid);
-  }else{
+  }
+  else
+  {
     // error
   }
 }
 
-int main(){
+int main()
+{
   int h;
   getHeight(&h);
-  for(;h>=0;h--){
+  for (; h >= 0; h--)
+  {
     //printf("%d", h);
     createProcess();
   }
